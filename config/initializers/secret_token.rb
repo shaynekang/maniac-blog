59d81d184811949cd1bd7865eab95924690c97cd
paranoiase <paranoiase@gmail.com>
2011-07-23 13:53:13 +0900

    first release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManiacBlog::Application.config.secret_token = 'ef5f1439c0e981e26af2454d47cdeae7cea4942523ebda670dd11abfb5f2f21192921362bae27da45a3a0e64decdefc25710f12ea6899b707902ab17a6ced291'
